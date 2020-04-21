5bc4dfbd8517fc0a611e4f7c6ef6695124331538
Luan Castheloge <luanccp@gmail.com>
2020-04-21 11:52:52 -0300

    style: get repo data from api

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
-
+import api from '../../services/api'
 
 import logo from '../../assets/logo.svg';
 
@@ -10,9 +10,41 @@ interface RepositoryParams {
   repository: string;
 }
 
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  }
+}
+
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
 
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then(response => {
+      setRepository(response.data);
+    })
+
+    api.get(`repos/${params.repository}/issues`).then(response => {
+      setIssues(response.data);
+    })
+  }, [params.repository])
   return (
     <>
       <Header>
@@ -22,40 +54,44 @@ const Repository: React.FC = () => {
         Voltar
       </Link>
       </Header>
+      {repository && (
 
-      <RepositoryInfo>
+        <RepositoryInfo>
         <header>
-          <img src="teste" alt="testes"/>
+          <img src={repository.owner.avatar_url} alt={repository?.owner.login} />
           <div>
-            <strong>nome do repo</strong>
-            <p>Descricao do repo</p>
+            <strong>{repository.full_name}</strong>
+            <p>{repository.description}</p>
           </div>
         </header>
         <ul>
           <li>
-            <strong>821</strong>
+            <strong>{repository.stargazers_count}</strong>
             <p>Stars</p>
           </li>
           <li>
-            <strong>821</strong>
+            <strong>{repository.forks_count}</strong>
             <p>Forks</p>
           </li>
           <li>
-            <strong>821</strong>
+      <strong>{repository.open_issues_count}</strong>
             <p>Issues abertas</p>
           </li>
         </ul>
       </RepositoryInfo>
+      )}
 
       <Issues>
-        {/* <Link key={repo.full_name} to={`/repository/${repo.full_name}`}> */}
-        <Link to="asdasd">
-          <div>
-            <strong>repo.full_name</strong>
-            <p>repo.description</p>
-          </div>
-          <FiChevronRight size={20} />
-        </Link>
+        {issues.map(issue =>(
+          <a key={issue.id} href={issue.html_url}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+
+        ))}
       </Issues>
     </>
   )
